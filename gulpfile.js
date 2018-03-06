45160303ee1277297f3321283e8e1262904e3e43
akwright <alexander.k.wright@gmail.com>
2018-03-05 19:57:35 -0500

    Modified the gulpfile to fix browsersync with nunjucks. Removed superfluous packages and gulp tasks.

--- gulpfile.js
+++ gulpfile.js
@@ -1,91 +1,69 @@
 const gulp = require('gulp');
 const plugins = require('gulp-load-plugins');
-const critical = require('critical').stream;
 const autoprefixer = require('autoprefixer');
-const browserSync = require('browser-sync');
-const browserify = require('browserify');
-const buffer = require('vinyl-buffer');
-const cache = require('gulp-cache');
-const imagemin = require('gulp-imagemin');
-const notify = require('gulp-notify');
-const source = require('vinyl-source-stream');
-const fs = require('fs');
+const browserSync = require('browser-sync').create();
+const reload = browserSync.reload;
 
 const paths = {
-  css: {
-    src: './src/scss/',
-    dest: './public/css/'
-  },
-  images: {
-    src: './src/img/**/*',
-    dest: './public/img/'
-  }
-};
+  scss: 'src/scss/**/*.scss',
+  css: 'public/css/',
+  njk: 'templates/**/*.njk',
+  html: 'pages/**/*.html',
+  dist: 'public/'
+}
 
 
 /* ----------------- */
 /* Development
 /* ----------------- */
 
-gulp.task('development', ['templates', 'styles'], () => {
-  browserSync({
-    server: {
-      baseDir: './public/'
-    },
-    open: false,
-    online: false,
-    notify: false,
-    snippetOptions: {
-      rule: {
-        match: /<\/body>/i,
-        fn: (snippet) => snippet
-      }
-    }
+gulp.task('dev', ['scss', 'nunjucks'], () => {
+  browserSync.init({
+    server: './public/', open: false, online: false, notify: true
   });
-
-  gulp.watch(paths.css.src + '**/*.scss', ['styles']);
-  gulp.watch('./pages/**/*.html', ['templates']);
+  
+  gulp.watch(paths.njk, ['nunjucks']);
+  gulp.watch(paths.scss, ['scss']);
+  gulp.watch(paths.html, ['nunjucks']).on('change', reload);
 });
 
 
-/* ----------------- */
-/* Templates
-/* ----------------- */
-gulp.task('templates', () => {
-  return gulp.src('./pages/**/*.+(html|nunjucks)')
+// Converting njk files to html
+gulp.task('nunjucks', () => {
+  return gulp.src(paths.html)
     .pipe(plugins().data(function() {
       return require('./src/data.json')
     }))
     .pipe(plugins().nunjucksRender({
-      path: ['./templates/']
+      path: ['templates/']
     }))
     .pipe(gulp.dest('public'))
-    .pipe(browserSync.stream());
+    .pipe(reload({stream: true }));
 });
 
 
-/* ----------------- */
-/* Styles
-/* ----------------- */
+// HTML processing
+gulp.task('html', () => {
+  return gulp.src(paths.html)
+    .pipe(newer(paths.html))
+    .pipe(gulp.dest(paths.dist));
+});
 
-gulp.task('styles', () => {
-  return gulp.src(paths.css.src + '**/*.scss')
+
+// Converting scss files to css files
+gulp.task('scss', () => {
+  return gulp.src(paths.scss)
     .pipe(plugins().sassGlob())
-    .pipe(plugins().sourcemaps.init())
     .pipe(plugins().postcss([
       autoprefixer({ browsers: ['last 2 versions'] })
     ], { syntax: require('postcss-scss') }))
     .pipe(plugins().sass().on('error', plugins().sass.logError))
-    .pipe(plugins().sourcemaps.write())
-    .pipe(gulp.dest(paths.css.dest))
-    .pipe(browserSync.stream());
+    .pipe(gulp.dest(paths.css))
+    .pipe(reload({stream: true }));
 });
 
 
-/* ----------------- */
-/* Cssmin
-/* ----------------- */
-
+// Minify the CSS for production
 gulp.task('cssmin', () => {
   return gulp.src(paths.css.src + '**/*.scss')
     .pipe(plugins().sassGlob())
@@ -95,19 +73,9 @@ gulp.task('cssmin', () => {
     .pipe(gulp.dest(paths.css.dest));
 });
 
-
-// Generate & Inline Critical-path CSS
-gulp.task('critical', function () {
-  return gulp.src('public/*.html')
-    .pipe(critical({base: 'public/', inline: true, minify: true, css: ['public/css/georgefoxalumni.css']}))
-    .on('error', function(err) { gutil.log(gutil.colors.red(err.message)); })
-    .pipe(gulp.dest('public'));
-});
-
 /* ----------------- */
 /* Taks
 /* ----------------- */
 
-gulp.task('default', ['development']);
-gulp.task('deploy', ['cssmin']);
-gulp.task('crit', ['critical']);
+gulp.task('default', ['dev']);
+gulp.task('deploy', ['nunjucks', 'html', 'cssmin']);
